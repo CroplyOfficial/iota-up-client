e891108f7dfd1fec2333b12925ed37c93de70007
Robert Bornschein <robertbornschein@mailbox.tu-dreden.de>
2021-07-01 16:00:08 +0200

    🎨 palette adjustments

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -1,13 +1,11 @@
 import {createMuiTheme, responsiveFontSizes} from "@material-ui/core";
-import {green, lightGreen} from "@material-ui/core/colors";
+import {green} from "@material-ui/core/colors";
 
 const palette = {
   primary: {
-    main: green[500]
+    main: "#1B8271",
   },
-  secondary: {
-    main: lightGreen[500]
-  }
+  secondary: green
 }
 const _theme = createMuiTheme({palette});
 export const theme = responsiveFontSizes(_theme);
