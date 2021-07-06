4b3769879f627a04def18cf20d422ab522d30a74
Robert Bornschein <robertbornschein@mailbox.tu-dreden.de>
2021-07-06 11:15:20 +0200

    added categoriesCard state

--- src/pages/root/sampleState.tsx
+++ src/pages/root/sampleState.tsx
@@ -1,9 +1,27 @@
-import {IProject} from "../../interfaces/project.interface";
+import {
+  Add,
+  Book,
+  FilterFrames,
+  HelpTwoTone,
+  Close,
+  RecentActors,
+} from "@material-ui/icons";
+import { makeStyles, createStyles } from "@material-ui/core";
+import { ICard } from "../../interfaces/categoriesCard.interface";
+import { IProject } from "../../interfaces/project.interface";
 
-export const SampleProjects: IProject[] = [{
+export const SampleProjects: IProject[] = [
+  {
     title: "IOTA MEMES - fun community meme website",
     description: "Long project description",
-    images: ["https://live.staticflickr.com/7629/28393379471_fd6ce887c3_b.jpg", "", "", "", "", ""],
+    images: [
+      "https://live.staticflickr.com/7629/28393379471_fd6ce887c3_b.jpg",
+      "",
+      "",
+      "",
+      "",
+      "",
+    ],
     tags: [],
     donations: 143,
     upvotes: 2132,
@@ -12,13 +30,18 @@ export const SampleProjects: IProject[] = [{
     funding: [0, 100],
     milestones: [],
     created_by: "abc",
-
-    
   },
-    { 
+  {
     title: "Identity Suite - Open Source Application Blueprints",
     description: "Long project description",
-    images: ["https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1109&q=80", "", "", "", "", ""],
+    images: [
+      "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1109&q=80",
+      "",
+      "",
+      "",
+      "",
+      "",
+    ],
     tags: ["technology"],
     donations: 143,
     upvotes: 2132,
@@ -28,10 +51,17 @@ export const SampleProjects: IProject[] = [{
     milestones: [],
     created_by: "abc",
   },
-    {
+  {
     title: "IOTA Live - Deep Dive Web Series with IF & Community Members",
     description: "Long project description",
-    images: ["https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80", "", "", "", "", ""],
+    images: [
+      "https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
+      "",
+      "",
+      "",
+      "",
+      "",
+    ],
     tags: ["creative"],
     donations: 143,
     upvotes: 2132,
@@ -41,7 +71,34 @@ export const SampleProjects: IProject[] = [{
     milestones: [],
     created_by: "abc",
   },
+];
 
-
-  ]
-
+const style = { fontSize: "90px" };
+export const SampleCategorieCards: Array<ICard> = [
+  {
+    icon: <Add color="primary" style={style} />,
+    title: "Add",
+  },
+  {
+    icon: <Book color="primary" style={style} />,
+    title: "Book",
+  },
+  {
+    icon: <FilterFrames color="primary" style={style} />,
+    title: "Filter",
+  },
+  {
+    icon: <HelpTwoTone color="primary" style={style} />,
+    title: "Help",
+  },
+  {
+    icon: <Close color="primary" style={style} />,
+    title: "Close",
+  },
+  /*
+   {
+    icon: <RecentActors color="primary" style={style} />,
+    title: "Actors",
+  },
+   */
+];
