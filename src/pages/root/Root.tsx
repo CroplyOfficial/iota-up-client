33f6cea87ca8580ce51c732a67e049c873d50698
Robert Bornschein <robertbornschein@mailbox.tu-dreden.de>
2021-07-06 11:16:18 +0200

    🚧 using categories section / missing scroll bar

--- src/pages/root/Root.tsx
+++ src/pages/root/Root.tsx
@@ -1,24 +1,28 @@
-import {makeStyles, createStyles} from "@material-ui/core";
+import { makeStyles, createStyles } from "@material-ui/core";
 import React from "react";
-import {Container} from "../../components/container/container";
-import {Navbar} from "../../components/navbar/Navbar";
+import { Container } from "../../components/container/container";
+import { Navbar } from "../../components/navbar/Navbar";
 import "../../static/css/root/root.css";
-import {FeaturedSection} from "./featuredSection.root";
-import {Hero} from "./hero.root";
+import { FeaturedSection } from "./featuredSection.root";
+import { Hero } from "./hero.root";
 import { Counter } from "./counter.root";
-import { SampleProjects } from "./sampleState";
+import { SampleProjects, SampleCategorieCards } from "./sampleState";
+import { Categories } from "./categories/categories.root";
+import { ICard } from "../../interfaces/categoriesCard.interface";
 
-const useStyles = makeStyles(()=>createStyles({
-  heroContainer: {
-    paddingBottom: "30vh",
-  },
-  featuredSection: {
-    paddingBottom: "107px",
-  },
-  lastSection: {
-    paddingBottom: "107px",
-  }
-}))
+const useStyles = makeStyles(() =>
+  createStyles({
+    heroContainer: {
+      paddingBottom: "30vh",
+    },
+    featuredSection: {
+      paddingBottom: "107px",
+    },
+    lastSection: {
+      paddingBottom: "107px",
+    },
+  })
+);
 
 export const Root = () => {
   const classes = useStyles();
@@ -27,26 +31,52 @@ export const Root = () => {
   const featuredTitle = "Projects on the UP";
   const featuredSubHeader = (
     <div>
-    Some of the top voted for and supported projects
-    <br/>
-accross all categories from the UP community
-  <br/>
-creators, designers, and developers
-  </div>);
-  const featuredProjects = [...SampleProjects].sort(() => Math.random() - 0.5 );
+      Some of the top voted for and supported projects
+      <br />
+      accross all categories from the UP community
+      <br />
+      creators, designers, and developers
+    </div>
+  );
+  const featuredProjects = [...SampleProjects].sort(() => Math.random() - 0.5);
   const featuredOnClick = () => null;
   /* FEATURED */
 
   /* LATEST */
   const latestTitle = "Latest Projects";
-  const latestSubHeader = (<div>
-    Freshly launched community projects that
-    <br/>
-    are looking for UP votes and support
-    </div>);
-  const latestProjects = [...SampleProjects].sort(() => Math.random() - 0.5 );
+  const latestSubHeader = (
+    <div>
+      Freshly launched community projects that
+      <br />
+      are looking for UP votes and support
+    </div>
+  );
+  const latestProjects = [...SampleProjects].sort(() => Math.random() - 0.5);
   const latestOnClick = () => null;
   /* LATEST */
+
+  /* Cards */
+  const categoriesPreHeader = "categories";
+  const categoriesTitle = (
+    <div>
+      Explore the Community
+      <br />
+      Discover the Future.
+    </div>
+  );
+  const categoriesSubHeader = (
+    <div>
+      Looking to support specific projects?
+      <br />
+      Check out the latest and most UP voted
+      <br />
+      community projects by category.
+    </div>
+  );
+  const categoriesCards: Array<ICard> = [...SampleCategorieCards].sort(
+    () => Math.random() - 0.5
+  );
+  /* Cards */
 
   return (
     <React.Fragment>
@@ -55,11 +85,31 @@ creators, designers, and developers
         <Hero />
       </Container>
       <Container>
-        <FeaturedSection className={classes.featuredSection} title={featuredTitle} subHeader={featuredSubHeader} projects={featuredProjects} onClick={featuredOnClick} />
-        <FeaturedSection className={classes.lastSection} title={latestTitle} subHeader={latestSubHeader} projects={latestProjects} onClick={latestOnClick} />
+        <FeaturedSection
+          className={classes.featuredSection}
+          title={featuredTitle}
+          subHeader={featuredSubHeader}
+          projects={featuredProjects}
+          onClick={featuredOnClick}
+        />
+        <FeaturedSection
+          className={classes.lastSection}
+          title={latestTitle}
+          subHeader={latestSubHeader}
+          projects={latestProjects}
+          onClick={latestOnClick}
+        />
       </Container>
-      <Container> 
+      <Container>
         <Counter />
+      </Container>
+      <Container>
+        <Categories
+          preHeader={categoriesPreHeader}
+          title={categoriesTitle}
+          subHeader={categoriesSubHeader}
+          cards={categoriesCards}
+        />
       </Container>
     </React.Fragment>
   );
