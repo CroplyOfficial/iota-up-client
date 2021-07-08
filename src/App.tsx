e22d1f6ec91b487f7166dbc1e965c6db551bf328
Robert Bornschein <robertbornschein@mailbox.tu-dreden.de>
2021-07-08 18:05:16 +0200

    added /about to the router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import {Root} from "./pages/root/Root";
-import {ThemeProvider} from '@material-ui/core';
-import {theme} from './theme/theme';
+import { Root } from "./pages/root/Root";
+import { ThemeProvider } from "@material-ui/core";
+import { theme } from "./theme/theme";
+import { AboutUs } from "./pages/about_us/AboutUs";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <ThemeProvider theme={theme}>
         <Router>
           <Switch>
-            <Route path="/" exact render={() => <Root/>} />
+            <Route path="/" exact render={() => <Root />} />
+            <Route path="/about" exact render={() => <AboutUs />} />
           </Switch>
         </Router>
       </ThemeProvider>
