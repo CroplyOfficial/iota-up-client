67c6b2933a181c8b2f4c85b6c60c8d17752a6749
Coodos <info@coodos.co>
2021-08-08 10:22:04 +0530

    added reducers for getting 'my projects'

--- src/constants/projectConstants.tsx
+++ src/constants/projectConstants.tsx
@@ -13,3 +13,7 @@ export const GET_RECOMMENDED_FAIL: string = "GET_RECOMMENDED_FAIL";
 export const GET_LATEST_REQUEST: string = "GET_LATEST_REQUEST";
 export const GET_LATEST_SUCCESS: string = "GET_LATEST_SUCCESS";
 export const GET_LATEST_FAIL: string = "GET_LATEST_FAIL";
+
+export const GET_MY_PROJECTS_REQUEST: string = "GET_MY_PROJECTS_REQUEST";
+export const GET_MY_PROJECTS_SUCCESS: string = "GET_MY_PROJECTS_SUCCESS";
+export const GET_MY_PROJECTS_FAIL: string = "GET_MY_PROJECTS_FAIL";
--- src/reducers/projectReducers.tsx
+++ src/reducers/projectReducers.tsx
@@ -11,6 +11,9 @@ import {
   GET_LATEST_REQUEST,
   GET_LATEST_SUCCESS,
   GET_LATEST_FAIL,
+  GET_MY_PROJECTS_REQUEST,
+  GET_MY_PROJECTS_SUCCESS,
+  GET_MY_PROJECTS_FAIL,
 } from "../constants/projectConstants";
 
 export const loadProjectsReducer = (state = {}, action: any) => {
@@ -59,6 +62,19 @@ export const loadLatestProjectsReducer = (state = {}, action: any) => {
     case GET_LATEST_SUCCESS:
       return { loading: false, latestProjects: action.payload };
     case GET_LATEST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const loadMyProjectsReducer = (state = {}, action: any) => {
+  switch (action.type) {
+    case GET_MY_PROJECTS_REQUEST:
+      return { loading: true };
+    case GET_MY_PROJECTS_SUCCESS:
+      return { loading: false, myProjects: action.payload };
+    case GET_MY_PROJECTS_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
