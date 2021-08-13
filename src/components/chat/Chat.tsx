4c5bb4e6e4c3f01da9dccf7f9447fa01644198be
Robert Bornschein <robertbornschein@mailbox.tu-dreden.de>
2021-08-13 09:35:03 +0200

    added react matrix components

---
+++ src/components/chat/Chat.tsx
@@ -0,0 +1,22 @@
+import { makeStyles, createStyles } from "@material-ui/core";
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      position: "fixed",
+      bottom: 0,
+      right: 0,
+      transform: "translateX(-10px)",
+      width: "288px",
+      height: "48px",
+      backgroundColor: "white",
+      boxShadow: "0 3px 10px rgb(0 0 0 / 0.2)",
+      MozBorderRadiusTopLeft: "10px",
+      MozBorderRadiusTopRight: "10px",
+    },
+  })
+);
+export const Chat = () => {
+  const classes = useStyles();
+
+  return <div className={classes.root}></div>;
+};
