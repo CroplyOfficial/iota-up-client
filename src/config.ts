09c0cff32805b0f173240b4cafb0d1369af4f3a6
Robert Bornschein <robertbornschein@mailbox.tu-dreden.de>
2021-08-14 16:10:26 +0200

    fixes plus verifyDelete Modal

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ import fallback5 from "./static/images/placeholder/placeholder_5.jpg";
 const fallbackImages = [fallback1, fallback2, fallback3, fallback4, fallback5];
 export const useFallbackImage = () =>
   fallbackImages[Math.floor(Math.random() * fallbackImages.length)];
-export const API = "http://api.iotaup.com/api";
+export const API = "http://localhost:5000/api";
 export const fallbackProjectImage =
   "https://images.unsplash.com/photo-1604966795869-8df0b4517138?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80";
 export const iotaWalletAdress = "iota:123123123";
