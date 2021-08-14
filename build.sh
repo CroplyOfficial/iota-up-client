a7d04553221da54516a336bc0864e7d1fb379729
coodos <69296233+coodos@users.noreply.github.com>
2021-08-14 12:00:33 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 echo "installing packages..."
 npm ci 
 echo "creating prod build..."
-npm run buildsh
+npm run build
 echo "stop existing deployment..."
 pm2 delete deploy
 echo "deploy built frontend to prod..."
