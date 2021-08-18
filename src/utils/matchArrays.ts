b924b223fa812237b404bb8dc1c2f8d80c2c7ed1
Coodos <info@coodos.co>
2021-08-18 14:20:30 +0530

    fixed filters bug

--- src/utils/matchArrays.ts
+++ src/utils/matchArrays.ts
@@ -11,7 +11,7 @@ const compareArrays = (array1: any[], array2: any[]): any[] => {
   const matches: any[] = [];
   array1.forEach((item1) => {
     array2.forEach((item2) => {
-      if (item1 === item2) {
+      if (decodeURI(item1.toLowerCase()) === decodeURI(item2.toLowerCase())) {
         matches.push(item1);
       }
     });
