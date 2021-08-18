1f8f780f315dc8aaf1f5cf4a9f95f54ec3deac1e
coodos <69296233+coodos@users.noreply.github.com>
2021-08-18 14:32:56 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ cat ./src/config.ts | sed "s/localhost:5000/api.iotaup.com/" > ./src/config.temp
 rm ./src/config.ts 
 mv ./src/config.temp ./src/config.ts
 echo "installing packages..."
-npm ci 
+npm i 
 echo "creating prod build..."
 npm run build
 echo "stop existing deployment..."
