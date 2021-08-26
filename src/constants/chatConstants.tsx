56798369e44aff7efebd016d02e804be0edea0ed
coodos <info@coodos.co>
2021-08-26 10:26:57 +0530

    added chat

---
+++ src/constants/chatConstants.tsx
@@ -0,0 +1,7 @@
+export const GET_CHATS_REQUEST: string = "GET_CHATS_REQUEST";
+export const GET_CHATS_SUCCESS: string = "GET_CHATS_SUCCESS";
+export const GET_CHATS_FAIL: string = "GET_CHATS_FAIL";
+
+export const GET_CHAT_REQUEST: string = "GET_CHAT_REQUEST";
+export const GET_CHAT_SUCCESS: string = "GET_CHAT_SUCCESS";
+export const GET_CHAT_FAIL: string = "GET_CHAT_FAIL";
