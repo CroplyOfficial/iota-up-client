39061ebd4707c8c81b6923c1b54efac8e0a9943f
coodos <info@coodos.co>
2021-08-31 18:55:23 +0530

    favorites

--- src/pages/dashboard/tabs/favorites.dashboard.tsx
+++ src/pages/dashboard/tabs/favorites.dashboard.tsx
@@ -2,6 +2,10 @@ import { createStyles, makeStyles, Button } from "@material-ui/core";
 import { Card2 } from "../../../components/card/card2";
 import { IProject } from "../../../interfaces/project.interface";
 import { AddNewProjectCard } from "../AddNewProjectCard.dashboard";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { RootState } from "../../../store";
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -29,19 +33,36 @@ const useStyles = makeStyles(() =>
     },
   })
 );
-interface IProps {
-  projects?: IProject[];
-}
+interface IProps {}
 export const DashboardFavorites = (props: IProps) => {
-  const { projects } = props;
+  const [projects, setProjects] = useState<IProject[]>();
+
+  const userMeta = useSelector((state: RootState) => state.userLogin);
+  const { userInfo }: any = userMeta;
+
   const classes = useStyles();
-  const handleAddNewProject = () => null;
+
+  const getFavorites = async () => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo?.token}`,
+      },
+    };
+    const { data } = await axios.get("/api/users/me/favorites", config);
+    setProjects(data);
+  };
+
+  useEffect(() => {
+    getFavorites();
+  }, []);
+
   return (
     <div>
       <div className={classes.root}>
-        {/* {SampleProjects.map((p) => (
+        {projects?.map((p) => (
           <Card2 project={p} />
-        ))} */}
+        ))}
         <Button
           color="secondary"
           variant="contained"
