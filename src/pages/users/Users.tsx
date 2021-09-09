0900c0a862163a10580303a0541da919e94d7f17
coodos <info@coodos.co>
2021-09-09 12:41:26 +0530

    added admin users stuff

---
+++ src/pages/users/Users.tsx
@@ -0,0 +1,109 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
+import { IUser } from "../../interfaces/user.interface";
+import { Modal, Button } from "@material-ui/core";
+import "../infractions/infractions.css";
+
+export const Users = () => {
+  const [users, setUsers] = useState<IUser[]>([]);
+  const userInfoMeta = useSelector((state: RootState) => state.userLogin);
+  const { userInfo }: any = userInfoMeta;
+  const [open, setOpen] = useState<boolean>(false);
+
+  const getUsers = async () => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const infractions: any = await axios.get("/api/admin/list-users", config);
+    setUsers(infractions.data);
+  };
+
+  const handleBanUser = async (id: string) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    await axios.get(`/api/admin/ban-user-by-id/${id}`, config);
+    await getUsers();
+  };
+
+  const [action, setAction] = useState<string>();
+  const [id, setId] = useState<string>();
+
+  const commitAction = (act: string, id: string) => {
+    switch (act) {
+      case "ban":
+        handleBanUser(id);
+        setId("");
+        setAction("");
+        setOpen(false);
+        return;
+      default:
+        return;
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+      >
+        <div className="modal">
+          <h1>Confirm {action}</h1>
+          <p>are you sure you want to go forward?</p>
+          <Button
+            variant="outlined"
+            color="secondary"
+            onClick={() => {
+              if (!(action && id)) return;
+              commitAction(action, id);
+            }}
+          >
+            CONFIRM
+          </Button>
+        </div>
+      </Modal>
+      <table className="infractions">
+        <tr>
+          <th>User</th>
+          <th>isBanned</th>
+          <th>isAdmin</th>
+          <th>Actions</th>
+        </tr>
+        {users &&
+          users?.map((user: IUser) => (
+            <tr>
+              <td>{`${user.firstName} ${user.lastName} (${user.email})`}</td>
+              <td>{String(user.isBanned)}</td>
+              <td>{String(user.isAdmin)}</td>
+              <td>
+                <button
+                  onClick={() => {
+                    setId(user._id);
+                    setAction("ban");
+                    setOpen(true);
+                  }}
+                >
+                  Ban User
+                </button>
+              </td>
+            </tr>
+          ))}
+      </table>
+    </div>
+  );
+};
